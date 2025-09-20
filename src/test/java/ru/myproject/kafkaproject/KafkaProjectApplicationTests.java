35446c2d9e3ddaf8e26e16dd66d35e9f53435d6e
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2025-09-21 01:45:17 +0300

    develop - Initial project

---
+++ src/test/java/ru/myproject/kafkaproject/KafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.myproject.kafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
